dc2d65a27706a2ebca8b3b6eba381e24bc7a1af3
Isaque Akamine <isaqueakamine@MacBook-Pro.local>
2020-10-12 11:56:05 -0300

    adjust for lottie at android platform

--- src/screens/Loading.tsx
+++ src/screens/Loading.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 export default function Loading() {
     return (
         <View style={styles.animationContainer}>
-            <LottieView
-                autoPlay
-                style={{
-                    width: 400,
-                    height: 400,
-                    backgroundColor: '#eee',
-                }}
-                source={require('../assets/loading.json')}
-            />
+            {Platform.OS === 'ios' ?
+                <LottieView
+                    autoPlay
+                    style={{
+                        width: 400,
+                        height: 400,
+                        backgroundColor: '#eee',
+                    }}
+                    source={require('../assets/loading.json')}
+                />
+                :
+                <Text>Loading...</Text>
+            }
         </View>
     );
 }
