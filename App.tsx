5008298a07ffda853656a4aa8fd1289d089c088d
Isaque Akamine <isaqueakamine@MacBook-Pro.local>
2020-10-13 08:54:02 -0300

    initializa firebase

--- App.tsx
+++ App.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import { View, StyleSheet } from "react-native";
 import Routes from './src/Navigation/routes';
 
+import * as  firebase from 'firebase';
+import { firebaseConfig } from './src/config';
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
