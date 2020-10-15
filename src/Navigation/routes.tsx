1bf594d459c707eb7f68f317a7a958459bbc95f7
Isaque Akamine <isaqueakamine@MacBook-Pro.local>
2020-10-15 12:52:46 -0300

    remove header from route stack navigation

--- src/Navigation/routes.tsx
+++ src/Navigation/routes.tsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Loading from '../screens/Loading';
-import Dashboard from '../screens/Dashboard';
-import Login from '../screens/Login';
-import Register from '../screens/Register';
+import Loading from "../screens/Loading";
+import Dashboard from "../screens/Dashboard";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
 
 const Stack = createStackNavigator();
 
 function Routes() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    setTimeout(() => { setLoading(false) }, 3500)
+  setTimeout(() => {
+    setLoading(false);
+  }, 3500);
 
-    if (loading) {
-        return (
-            <Loading />
-        )
-    }
+  if (loading) {
+    return <Loading />;
+  }
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen name="Dashboard" component={Dashboard} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" headerMode="none">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default Routes;
